e60222771e4fc3ff4080fe2514c7e9cb29babba5
yasuomaidana <yasuomaidana@gmail.com>
2026-04-16 07:52:48 -0600

    chore: add initial state management and view components

--- src/views.rs
+++ src/views.rs
@@ -1,6 +1,8 @@
 use crate::messages::Message;
 use crate::states::Counter;
-use iced::widget::{Column, button, column, text};
+use iced::alignment::Horizontal;
+use iced::widget::{Button, Column, button, column, text};
+use iced::{Fill, Font};
 
 pub trait ColumnView<T> {
     // &'_ self means self is borrowed for some lifetime chosen by the compiler.
@@ -8,17 +10,43 @@ pub trait ColumnView<T> {
     fn view(&'_ self) -> Column<'_, T>;
 }
 
+fn controller_button(content: &str, message: Message) -> Button<'_, Message> {
+    button(
+        text(content)
+            .font(Font::MONOSPACE)
+            .size(35)
+            .width(Fill)
+            .height(Fill)
+            .center(),
+    )
+    .on_press(message)
+    .width(Fill)
+}
+
 impl ColumnView<Message> for Counter {
     fn view(&'_ self) -> Column<'_, Message> {
         // buttons
-        let increment = button("+").on_press(Message::Increment);
-        let decrement = button("-").on_press(Message::Decrement);
+        let increment = controller_button("+", Message::Increment);
+
+        let decrement = controller_button("-", Message::Decrement);
 
         // display
-        let counter = text(self.value);
+        let counter = text(self.value)
+            // .style(|theme: &Theme| text::Style {
+            //     color: Some(theme.palette().primary),
+            // })
+            .style(text::warning)
+            .size(50)
+            .width(Fill)
+            .height(Fill)
+            .center();
 
         //layout
         let interface = column![increment, counter, decrement,];
         interface
+            .spacing(10)
+            .align_x(Horizontal::Center)
+            .width(Fill)
+            .height(Fill)
     }
 }
